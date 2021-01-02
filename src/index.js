2b54c6a9c292a7fa0c4528350e9c6ca853845b10
Matthew Hatcher <matthewh@halfpetal.com>
2021-01-02 16:44:50 -0500

    Adding glicko2

--- src/index.js
+++ src/index.js
@@ -29,3 +29,37 @@ for(let i = 0; i < 3; i++) {
         b.end();
     });
 }
+
+const glicko2 = require('glicko2');
+
+for(let i = 0; i < 3; i++) {
+    bench(`glicko2 rating period #${i+1}`, (b) => {
+        let playersJSON = JSON.parse(fs.readFileSync('data/players.json'));
+        let matches = JSON.parse(fs.readFileSync('data/matches.json'));
+
+        b.start();
+
+        let g2 = new glicko2.Glicko2({
+            tau : 0.5,
+            rating : 1500,
+            rd : 200,
+            vol : 0.06
+        });
+
+        let players = {};
+    
+        Object.keys(playersJSON).forEach((name) => {
+            players[name] = g2.makePlayer(playersJSON[name].rating, playersJSON[name].rd, playersJSON[name].sigma);
+        });
+
+        let rMatches = [];
+
+        matches[i].forEach((m) => {
+            rMatches.push([players[m.player1], players[m.player2], m.score]);
+        });
+    
+        g2.updateRatings(rMatches);
+
+        b.end();
+    });
+}
