2328d1b9577b0811360e3eb02d4a9bbec50c3b6d
Matthew Hatcher <matthewh@halfpetal.com>
2021-01-02 16:27:26 -0500

    Adding go-glicko.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ const bench = require('nanobench');
 const fs = require('fs');
 const GoGlicko = require('go-glicko');
 
-bench('go-glicko', (b) => {
-    let players = JSON.parse(fs.readFileSync('data/players.json'));
-    let matches = JSON.parse(fs.readFileSync('data/matches.json'));
-});
+for(let i = 0; i < 3; i++) {
+    bench(`go-glicko rating period #${i+1}`, (b) => {
+        let playersJSON = JSON.parse(fs.readFileSync('data/players.json'));
+        let matches = JSON.parse(fs.readFileSync('data/matches.json'));
+        let players = {};
+    
+        Object.keys(playersJSON).forEach((name) => {
+            players[name] = new GoGlicko.Player(new GoGlicko.Rating(playersJSON[name].rating, playersJSON[name].rd, playersJSON[name].sigma));
+        });
+        
+        b.start();
+        
+        let period = new GoGlicko.Period();
+    
+        Object.keys(players).forEach((name) => {
+            period.addPlayer(players[name]);
+        });
+    
+        matches[i].forEach((m) => {
+            period.addMatch(players[m.player1], players[m.player2], m.score);
+        });
+    
+        period.Calculate();
+    
+        b.end();
+    });
+}
